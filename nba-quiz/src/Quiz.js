9d834df6118e6c7761b85721e33bfe99593c5c8f
Lzdanowicz <lzdanowicz10@gmail.com>
2017-04-15 11:10:21 -0500

    Adding files

---
+++ nba-quiz/src/Quiz.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Quiz extends Component {
+
+
+render() {
+		return (
+			<div className="quiz">
+				<h1>Hello World</h1>
+			</div>
+		)
+	}
+}
+
+export default Quiz
